ebe6e46a3e15dd3e40df4082e27472274543198b
cindrekkakaur <117171647+cindrekkakaur@users.noreply.github.com>
2022-11-01 17:28:15 +0530

    Create ockStart.py

---
+++ ockStart.py
@@ -0,0 +1,20 @@
+Result = StringVar()
+
+def play():
+    user_pick = user_take.get()
+    if user_pick == comp_pick:
+        Result.set('tie,you both select same')
+    elif user_pick == 'rock' and comp_pick == 'paper':
+        Result.set('you loose,computer select paper')
+    elif user_pick == 'rock' and comp_pick == 'scissors':
+        Result.set('you win,computer select scissors')
+    elif user_pick == 'paper' and comp_pick == 'scissors':
+        Result.set('you loose,computer select scissors')
+    elif user_pick == 'paper' and comp_pick == 'rock':
+        Result.set('you win,computer select rock')
+    elif user_pick == 'scissors' and comp_pick == 'rock':
+        Result.set('you loose,computer select rock')
+    elif user_pick == 'scissors' and comp_pick == 'paper':
+        Result.set('you win ,computer select paper')
+    else:
+        Result.set('invalid: choose any one -- rock, paper, scissors')
